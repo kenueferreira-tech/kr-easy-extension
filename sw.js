5f5ae8be67eb58c4d29cbe88d4f3e36324274177
kenueferreira-tech <kenueferreira@gmail.com>
2026-09-11 10:04:01 -0300

    feat: transformar versão mobile em PWA instalável

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'kr-easy-v1';
+const APP_SHELL = ['/mobile.html', '/acesso.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).pathname.startsWith('/api/')) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('/mobile.html'))));
+});
